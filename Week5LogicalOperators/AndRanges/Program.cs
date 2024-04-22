b01b33bd03e84bd9264000e73da95737a3b5cdd3
Taavetl <163862539+Taavetl@users.noreply.github.com>
2024-04-22 15:07:22 +0300

    Add files via upload

---
+++ Week5LogicalOperators/AndRanges/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine(" mis temp on?");
+int temp = Int32.Parse(Console.ReadLine());
+
+if (temp >= 30) 
+{
+    Console.WriteLine("jube kuum");
+
+}
+else if (temp > 30 && temp >= 20)
+{
+
+    Console.WriteLine("kuum");
+}
+else if (temp < 20 && temp >= 15)
+{
+    Console.WriteLine("soe");
+
+}
+else if (temp < 15 && temp >= 10)
+{
+    Console.WriteLine(" jahe");
+}
+else if ( temp < 10 && temp > 0)
+{
+    Console.WriteLine("brrrrr");
+}
+else
+{
+    Console.WriteLine(" jube brrrr");
+
+}
---
+++ Week5LogicalOperators/Week5LogicalOperators/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int math, biology, chemistry;
+
+Console.WriteLine("Mata tulemus:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("bio tulemus:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Kema tulemus:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if (math >= 90 && biology >= 90 && chemistry >= 90)
+{
+    Console.WriteLine(" oled sees!");
+}
+else
+{
+    Console.WriteLine("luuser");
+}
